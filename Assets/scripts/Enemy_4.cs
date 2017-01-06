f57d42d044919bc917dca9347e06e2e59bce7779
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-01-06 21:22:15 +0000

    Create Enemy_4 behaviour

--- Assets/scripts/Enemy_4.cs
+++ Assets/scripts/Enemy_4.cs
@@ -1,7 +1,48 @@
 ﻿using UnityEngine;
 
-public class Enemy_4 : MonoBehaviour {
+public class Enemy_4 : Enemy {
 
-	
+    private Vector3 [] points;
+    private float timeStart;
+    private float duration = 4f;
+
+    private void Start ()
+    {
+        points = new Vector3 [2];
+        points [0] = Pos;
+        points [1] = Pos;
+
+        InitMovement ();
+    }
+
+    private void InitMovement ()
+    {
+        Vector3 p1 = Vector3.zero;
+        float esp = Main.Instance.EnemySpawnPadding;
+        Bounds cBounds = Utilities.CamBounds;
+
+        p1.x = Random.Range (cBounds.min.x + esp, cBounds.max.x - esp);
+        p1.y = Random.Range (cBounds.min.y + esp, cBounds.max.y - esp);
+
+        points [0] = points [1];
+        points [1] = p1;
+
+        timeStart = Time.time;
+    }
+
+    public override void Move ()
+    {
+        float u = (Time.time - timeStart) / duration;
+
+        if (u >= 1)
+        {
+            InitMovement ();
+            u = 0;
+        }
+
+        u = 1 - Mathf.Pow (1 - u, 2);
+
+        Pos = (1 - u) * points [0] + u * points [1];
+    }
 
 }
